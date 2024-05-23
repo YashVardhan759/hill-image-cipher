5bce0b1ee223d9c3862511f3b3e974b485bae691
YashVardhan759 <166308399+YashVardhan759@users.noreply.github.com>
2024-05-23 11:18:50 +0530

    Update image.py

--- image.py
+++ image.py
@@ -1,11 +1,14 @@
-from scipy import misc
+# from scipy import misc
 import numpy as np
 import matplotlib.pyplot as plt
+import imageio
+
 
 
 def read_image(image_path):
     """ Read an image and return a one hot vector of the image"""
-    img = misc.imread(image_path)
+    # img = misc.imread(image_path)
+    img = imageio.imread(image_path)
     reshape_value = 1
 
     for i in img.shape:
